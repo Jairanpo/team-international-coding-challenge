bcf09cac522e2672593fa67458fc537321cca9f2
Jair Anguiano <jairanpo@gmail.com>
2021-10-17 15:25:21 -0500

    fist commit

---
+++ data_capture.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+# Standard library:
+import pprint
+from functools import wraps
+
+# -- -- -- -- -- -- -- -- -- -- -- -- -- --
+# Third party:
+# -- -- -- -- -- -- -- -- -- -- -- -- -- --
+# project:
+# =============================================================================
+pp = lambda x, depth=6: pprint.PrettyPrinter(depth=depth).pprint(x)
+pf = pprint.pformat
+
+
+def validate_int_only_arguments(fn):
+    @wraps(fn)
+    def wrapper(*args):
+        list_of_values = args[1:]
+        if not all(isinstance(number, int) for number in list_of_values):
+            raise Exception(
+                f'< {fn.__name__} > only accepts integer arguments.'
+            )
+
+        return fn(*args)
+
+    return wrapper
+
+
+def validate_input_amount(amount):
+    def wrapper(fn):
+        @wraps(fn)
+        def inner(*args):
+            if len(args) != amount + 1:
+                raise Exception(
+                    f'< {fn.__name__} > require {amount} arguments.'
+                )
+
+            return fn(*args)
+
+        return inner
+
+    return wrapper
+
+
+class DataCapture(object):
+
+    def __init__(self):
+        self._list_of_values = []
+
+    @property
+    def values(self):
+        return self._list_of_values
+
+    @validate_input_amount(1)
+    @validate_int_only_arguments
+    def add(self, value):
+        self._list_of_values.append(value)
+
+    def build_stats(self):
+        if len(self._list_of_values) < 1:
+            raise Exception('You haven\'t add values')
+
+        self._list_of_values.sort()
+
+        _stats = {}
+        for i, n in enumerate(self._list_of_values):
+            if str(n) not in _stats:
+                _stats.update({
+                    str(n): i
+                })
+            else:
+                continue
+
+        return Stats(self._list_of_values, _stats)
+
+
+class Stats(object):
+    def __init__(self, values, stats):
+        self.values = values
+        self._stats = stats
+
+    @validate_input_amount(1)
+    @validate_int_only_arguments
+    def less(self, value):
+        _end = self._stats[str(value)]
+        return self.values[:_end]
+
+    @validate_input_amount(2)
+    @validate_int_only_arguments
+    def between(self, start, end):
+        _start = self._stats[str(start)]
+        _end = self._stats[str(end)] + 1
+
+        return self.values[_start: _end]
+
+    @validate_input_amount(1)
+    @validate_int_only_arguments
+    def greater(self, value):
+        _start = self._stats[str(value)] + 1
+        return self.values[_start:]
---
+++ tests/test_data_capture.py
@@ -0,0 +1,124 @@
+# Standard library:
+import pprint
+import unittest
+# -- -- -- -- -- -- -- -- -- -- -- -- -- --
+# Third party:
+# -- -- -- -- -- -- -- -- -- -- -- -- -- --
+# project:
+from data_capture import DataCapture
+
+# =============================================================================
+pp = lambda x, depth=6: pprint.PrettyPrinter(depth=depth).pprint(x)
+pf = pprint.pformat
+
+
+class TestDataCapture(unittest.TestCase):
+
+    def test_add_values(self):
+        dc = DataCapture()
+        dc.add(1)
+        dc.add(2)
+        self.assertEqual(len(dc.values), 2)
+
+    def test_using_wrong_amount_of_arguments(self):
+        capture = DataCapture()
+
+        with self.assertRaises(Exception) as context:
+            capture.add()
+
+        self.assertTrue('add' in str(context.exception))
+
+        amount_of_values = 10
+        # Create samples from 1 to 10
+        for n in range(1, amount_of_values + 1):
+            capture.add(n)
+
+        stats = capture.build_stats()
+        with self.assertRaises(Exception) as context:
+            stats.less()
+        self.assertTrue('less' in str(context.exception))
+
+        with self.assertRaises(Exception) as context:
+            stats.greater(3, 5, 5)
+        self.assertTrue('greater' in str(context.exception))
+
+        with self.assertRaises(Exception) as context:
+            stats.between(1, 2, 3)
+
+        self.assertTrue('between' in str(context.exception))
+
+    def test_using_wrong_data_type_arguments(self):
+        capture = DataCapture()
+
+        with self.assertRaises(Exception) as context:
+            capture.add('a')
+        self.assertTrue('add' in str(context.exception))
+
+        amount_of_values = 10
+        # Create samples from 1 to 10
+        for n in range(1, amount_of_values + 1):
+            capture.add(n)
+
+        stats = capture.build_stats()
+        with self.assertRaises(Exception) as context:
+            stats.less('a')
+        self.assertTrue('less' in str(context.exception))
+
+        with self.assertRaises(Exception) as context:
+            stats.greater('a')
+        self.assertTrue('greater' in str(context.exception))
+
+        with self.assertRaises(Exception) as context:
+            stats.between('a', 'b')
+
+        self.assertTrue('between' in str(context.exception))
+
+    def test_less_values(self):
+        capture = DataCapture()
+        amount_of_values = 10
+        for n in range(amount_of_values):
+            capture.add(n)
+
+        stats = capture.build_stats()
+
+        self.assertEqual(len(stats.less(3)), 3)
+
+    def test_greater_values(self):
+        capture = DataCapture()
+        amount_of_values = 10
+        sample = 3
+        # Create samples from 1 to 10
+        for n in range(1, amount_of_values + 1):
+            capture.add(n)
+
+        stats = capture.build_stats()
+        self.assertEqual(
+            len(stats.greater(sample)),
+            amount_of_values - sample
+        )
+
+    def test_between_values(self):
+        capture = DataCapture()
+
+        amount_of_values = 10
+        # Create samples from 1 to 10
+        for n in range(1, amount_of_values + 1):
+            capture.add(n)
+        stats = capture.build_stats()
+
+        self.assertEqual(len(stats.between(1, 10)), 10)
+        self.assertEqual(len(stats.between(1, 8)), 8)
+        self.assertEqual(len(stats.between(4, 5)), 2)
+        self.assertEqual(len(stats.between(5, 5)), 1)
+
+    def test_challenge_values(self):
+        capture = DataCapture()
+        capture.add(3)
+        capture.add(9)
+        capture.add(3)
+        capture.add(4)
+        capture.add(6)
+        stats = capture.build_stats()
+        self.assertEqual(stats.less(4), [3, 3])
+        self.assertEqual(stats.between(3, 6), [3, 3, 4, 6])
+        self.assertEqual(stats.greater(4), [6, 9])
